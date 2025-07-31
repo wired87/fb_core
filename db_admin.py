7a005870841ad93765384b5a35bc135a9cfb5bc6
wired87 <derbenedikt.sterra@gmail.com>
2025-07-31 08:15:24 +0200

    move sim

---
+++ db_admin.py
@@ -0,0 +1,51 @@
+import os
+import pprint
+
+from fb_core.real_time_database import FirebaseRTDBManager
+from qf_core_base import TEST_USER_ID, TEST_ENV_ID
+
+
+class DBAdmin:
+
+    def __init__(self, user_id=TEST_USER_ID, env_id=TEST_ENV_ID):
+        self.user_id = user_id
+        self.env_id = env_id
+        self.database = f"users/{self.user_id}/env/{self.env_id}"
+        self.metadata_path = "metadata"
+        self.states_path = "global_states"
+        self.db_manager = FirebaseRTDBManager(
+            database_url=os.environ.get("FIREBASE_RTDB"),
+            base_path=self.database,
+        )
+
+
+    def change_state(self):
+        """Changes state of ALL metadata entries"""
+        upsert_data = {}
+        data = self.db_manager.get_data(path=self.metadata_path)
+        #pprint.pp(data)
+        ready = None
+        for mid, data in data["metadata"].items():
+            current_state = data["status"]["state"]
+            if current_state == "active":
+                new_state = "inactive"
+                if ready is None:
+                    ready=False
+            else:
+                new_state = "active"
+                if ready is None:
+                    ready = True
+            upsert_data[f"{mid}/status/state/"] = new_state
+        pprint.pp(upsert_data)
+        self.db_manager.update_data(
+            path=self.metadata_path,
+            data=upsert_data
+        )
+        self.db_manager.update_data(
+            path=self.states_path,
+            data={"ready": ready}
+        )
+
+if __name__ == "__main__":
+    admin = DBAdmin()
+    admin.change_state()
