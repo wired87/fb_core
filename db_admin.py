40d775e410abbfde46e91c9f2f6610d778d11f7b
wired87 <derbenedikt.sterra@gmail.com>
2025-07-31 12:33:13 +0200

    debug

--- db_admin.py
+++ db_admin.py
@@ -19,24 +19,32 @@ class DBAdmin:
         )
 
 
-    def change_state(self):
+    def change_state(self, state=None):
         """Changes state of ALL metadata entries"""
         upsert_data = {}
         data = self.db_manager.get_data(path=self.metadata_path)
         #pprint.pp(data)
         ready = None
         for mid, data in data["metadata"].items():
-            current_state = data["status"]["state"]
-            if current_state == "active":
-                new_state = "inactive"
-                if ready is None:
-                    ready=False
+            if state is None:
+
+                current_state = data["status"]["state"]
+                if current_state == "active":
+                    new_state = "inactive"
+                    if ready is None:
+                        ready=False
+                else:
+                    new_state = "active"
+                    if ready is None:
+                        ready = True
             else:
-                new_state = "active"
-                if ready is None:
-                    ready = True
+                new_state = state
+                ready = False
+
             upsert_data[f"{mid}/status/state/"] = new_state
+
         pprint.pp(upsert_data)
+
         self.db_manager.update_data(
             path=self.metadata_path,
             data=upsert_data
